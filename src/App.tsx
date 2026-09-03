3d31e730b3e26999f89efb44ecd1b6306d5bf8bc
Wargas Teixeira <teixeira.wargas@gmail.com>
2026-09-03 19:27:32 -0300

    sql

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,79 @@
 import { open } from '@tauri-apps/plugin-dialog'
+import Database from '@tauri-apps/plugin-sql';
+import { BaseDirectory, readTextFile } from '@tauri-apps/plugin-fs'
+import * as path from '@tauri-apps/api/path'
 
 import { Button } from "#components/ui/button";
 import { Input } from "#components/ui/input";
 import { Field, FieldContent, FieldLabel } from "#components/ui/field";
 
 import "./App.css";
-import { useState } from 'react';
+import { startTransition, useState, useTransition } from 'react';
 import { InputGroup, InputGroupButton, InputGroupInput } from '#components/ui/input-group';
+import { readFileByLine } from '#lib/utils';
+import { Progress } from '#components/ui/progress';
 
 function App() {
 
-  const [files, setFiles] = useState<Record<string, string>>()
+  const [transition, setTransition] = useTransition()
+  const [files, setFiles] = useState<Record<string, string[]>>({})
+  const [cnpj, setCNPJ] = useState(``)
+  const [progress, setProgress] = useState(0)
+  const [countFiles, setCountFiles] = useState(0)
+  const [countReadFiles, setCountReadFiles] = useState(0)
+
+  async function processar() {
+
+    // for await(let file of files[``])
+    // const db = await Database.load(`sqlite:./${cnpj}.db`);
+
+    // await db.execute(`CREATE TABLE IF NOT EXISTS notas (id TEXT)`)
+
+    // const lista = await db.select(`SELECT * from notas`)
+
+    // const dir = await path.appDataDir()
+
+    // console.log({dir, lista})
+  }
+
+  async function processarSped() {
+
+
+    const filesSpeed = await open({
+      multiple: true,
+      directory: false
+    })
+
+    if (!filesSpeed) return;
+
+    setCountFiles(filesSpeed.length)
+
+    setCountReadFiles(0)
+
+    for await (const file of filesSpeed) {
+      setProgress(0)
+
+      await readFileByLine(file, async (line, size, bytesRead) => {
+
+        if (line.startsWith(`|`)) {
+          // console.log({ size, bytesRead }, (bytesRead / size).toLocaleString(`pt-BR`, { style: 'percent' }))
+        }
+
+        setProgress((bytesRead / size) * 100)
+
+      })
+
+      setCountReadFiles(c => c + 1)
+      setProgress(100)
+
+    }
+
+  }
 
 
   async function selectFile(key: string) {
     const file = await open({
-      multiple: false,
+      multiple: true,
       directory: false
     })
 
@@ -29,10 +87,10 @@ function App() {
     <main className="h-screen p-4">
       <div className="flex flex-col gap-4">
 
-       
+
         <Field>
           <FieldLabel>CNPJ</FieldLabel>
-          <Input />
+          <Input value={cnpj} onChange={i => setCNPJ(i.target.value)} />
         </Field>
 
         <Field>
@@ -49,7 +107,7 @@ function App() {
           <FieldLabel>NFE Emitidas</FieldLabel>
           <FieldContent>
             <InputGroup>
-              <InputGroupInput value={files?.['nfe-emitidas'] ?? ''}  />
+              <InputGroupInput value={files?.['nfe-emitidas'] ?? ''} />
               <InputGroupButton onClick={() => selectFile(`nfe-emitidas`)}>Selecione</InputGroupButton>
             </InputGroup>
           </FieldContent>
@@ -66,28 +124,30 @@ function App() {
         </Field>
 
         <Field>
-          <FieldLabel>SPED Entradas</FieldLabel>
+          <FieldLabel>SPED</FieldLabel>
           <FieldContent>
             <InputGroup>
-              <InputGroupInput value={files?.['sped-entradas'] ?? ''} />
-              <InputGroupButton onClick={() => selectFile(`sped-entradas`)}>Selecione</InputGroupButton>
+              <InputGroupInput value={files?.['sped'] ?? ''} />
+              <InputGroupButton onClick={() => processarSped()}>Selecione</InputGroupButton>
             </InputGroup>
+
+            <div className="flex flex-col gap-2 items-center mt-4">
+
+              <div>lendo {countReadFiles} de {countFiles}</div>
+
+              <Progress value={(countReadFiles / countFiles) * 100} />
+
+              <Progress value={progress} />
+            </div>
+
           </FieldContent>
         </Field>
-        <Field>
-          <FieldLabel>SPED Saidas</FieldLabel>
-          <FieldContent>
-            <InputGroup>
-              <InputGroupInput value={files?.['sped-saidas'] ?? ''} />
-              <InputGroupButton onClick={() => selectFile(`sped-saidas`)}>Selecione</InputGroupButton>
-            </InputGroup>
-          </FieldContent>
-        </Field>
 
 
-                
 
-        <Button variant={`default`}>Processar</Button>
+
+
+        <Button onClick={processar} variant={`default`}>Processar</Button>
       </div>
     </main>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,41 @@
+import { open } from "@tauri-apps/plugin-fs"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function readFileByLine(path: string, callback: (line: string, size: number, read: number) => Promise<void | null>) {
+
+  return new Promise(async acc => {
+
+    const handle = await open(path, { read: true })
+
+    const stat = await handle.stat()
+
+
+    const buffer = new Uint8Array(1024)
+    let leftOver = ``;
+    let fileBytesRead = 0
+
+    while (true) {
+      const bytesRead = await handle.read(buffer);
+      if (bytesRead == null || bytesRead == 0) break;
+
+      fileBytesRead += bytesRead
+
+      const chunk = leftOver + new TextDecoder().decode(buffer.subarray(0, bytesRead));
+
+      const lines = chunk.split('\n');
+
+      leftOver = lines.pop() ?? ''
+
+      for await (var line of lines) {
+        await callback(line, stat.size, fileBytesRead)
+      }
+    }
+
+    acc(true)
+  })
 }
