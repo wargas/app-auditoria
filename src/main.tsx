ae9223bf808167d3d88658349ce357671c1c4d21
Wargas Teixeira <teixeira.wargas@gmail.com>
2026-09-08 13:22:42 -0300

    logs

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,27 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { routes } from "./routes";
 import "./App.css";
 import { AppProvider } from "./app-context";
+import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log';
 
 const queryClient = new QueryClient()
+
+
+function forwardConsole(
+  fnName: 'log' | 'debug' | 'info' | 'warn' | 'error',
+  logger: (message: string) => Promise<void>
+) {
+  const original = console[fnName];
+  console[fnName] = (message) => {
+    original(message);
+    logger(message);
+  };
+}
+
+forwardConsole('log', trace);
+forwardConsole('debug', debug);
+forwardConsole('info', info);
+forwardConsole('warn', warn);
+forwardConsole('error', error);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
