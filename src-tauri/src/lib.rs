859843e5bdb0decdad466b8dba9f9d5e28dc3e34
Wargas Teixeira <teixeira.wargas@gmail.com>
2026-09-11 06:06:04 -0300

    refactor

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,25 @@
+use std::{fs::File, io::BufReader};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+
+#[tauri::command]
+fn list_zip_files(path: String) -> Result<Vec<String>, String> {
+    let file = File::open(path).map_err(|e| e.to_string())?;
+    let reader = BufReader::new(file);
+    let mut archive = zip::ZipArchive::new(reader).map_err(|e| e.to_string())?;
+    
+    let mut file_names = Vec::new();
+    for i in 0..archive.len() {
+        let file = archive.by_index(i).map_err(|e| e.to_string())?;
+        file_names.push(file.name().to_string());
+    }
+    
+    Ok(file_names)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -19,7 +37,7 @@ pub fn run() {
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, list_zip_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
