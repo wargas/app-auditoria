ec179b4d2ab5ff0df9ecb2aaf1dbf91805faa1c9
Wargas Teixeira <teixeira.wargas@gmail.com>
2026-09-15 10:24:27 -0300

    send-logs

--- src/app-context.tsx
+++ src/app-context.tsx
@@ -6,6 +6,7 @@ import { getCurrentWindow } from "@tauri-apps/api/window";
 import { open, save } from "@tauri-apps/plugin-dialog";
 import Database from "@tauri-apps/plugin-sql";
 import { sep, resolveResource } from "@tauri-apps/api/path"
+import { info } from "@tauri-apps/plugin-log"
 
 import { ComponentProps, createContext, useContext } from "react";
 import _ from "lodash";
@@ -28,12 +29,13 @@ export function AppProvider({ children }: ComponentProps<"div">) {
         queryFn: async () => {
             const path = await getProject()
 
-
             win.setTitle('auditoria')
 
             if (!path) return null;
 
             const fileName = _.last(path.split(sep()))
+
+            info(JSON.stringify({fileName, path}))
 
 
             if (fileName) {
@@ -41,6 +43,8 @@ export function AppProvider({ children }: ComponentProps<"div">) {
             }
 
             const exist = await exists(path)
+
+            info(JSON.stringify({fileName, exist}))
 
             if (!exist) {
                 toast.error("Arquivo nao encontrado")
@@ -51,6 +55,8 @@ export function AppProvider({ children }: ComponentProps<"div">) {
             }
 
             const db = await Database.load(path)
+
+            info(JSON.stringify({db}))
 
             // await initDb(db)
             return db;
